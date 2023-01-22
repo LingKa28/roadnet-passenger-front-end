524788ba287a0a830a5b8328a6fa1557f1a4a2ce
Ying <cnfty786@gmail.com>
2023-01-23 00:24:38 +0800

    feat：初始化主要页面并更新对应配置文件及 tabBar

---
+++ pages/card-voucher-page/card-voucher-page.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		card voucher page
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
